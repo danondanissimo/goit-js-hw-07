80da3bb70ddac33d00aea674046dd0bca5e9885b
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-01-02 20:04:21 +0000

    Finished task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  //   loginForm.elements;
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (email.length === 0 || password.length === 0) {
+    alert("All form fields must be filled in");
+  } else {
+    const loginDetails = { email, password };
+
+    console.log(loginDetails);
+  }
+  loginForm.reset();
+});
