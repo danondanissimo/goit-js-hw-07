6a36de00eadd2dca7fb6fd6fa556d9835272f78e
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-01-04 17:05:51 +0000

    Finished task 6 + slightly edited styles for other tasks

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonCreate = document.querySelector("[data-create]");
+
+const buttonDestroy = document.querySelector("[data-destroy]");
+
+const number = document.querySelector('[type="number"]');
+
+const boxesContainer = document.querySelector("#boxes");
+
+const boxes = document.querySelectorAll(".random-box");
+
+buttonCreate.addEventListener("click", (event) => {
+  if (number.value <= 100) {
+    createElem(number.value);
+  }
+  number.value = "";
+});
+
+buttonDestroy.addEventListener("click", () => {
+  boxesContainer.innerHTML = "";
+});
+
+function createElem(elemAmount) {
+  for (let i = 0; i < elemAmount; i += 1) {
+    const element = document.createElement("div");
+
+    element.style.height = `${30 + 10 * i}px`;
+    element.style.width = `${30 + 10 * i}px`;
+    element.style.backgroundColor = getRandomHexColor();
+
+    boxesContainer.prepend(element);
+  }
+}
