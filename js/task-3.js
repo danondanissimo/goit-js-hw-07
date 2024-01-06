5bc6c81bb73e05f0094436bc93a388e90d647b1a
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-01-06 15:02:39 +0000

    Update task 3

--- js/task-3.js
+++ js/task-3.js
@@ -6,12 +6,8 @@ inputName.addEventListener("input", () => {
   if (inputName.value.trim().length !== 0) {
     outputName.innerHTML = inputName.value.trim();
   } else {
-    outputName.value = "Anonymous";
-    console.log(outputName.innerHTML);
+    outputName.innerText = "Anonymous";
+
     inputName.value = "";
   }
-});
-
-inputName.addEventListener("change", () => {
-  inputName.value = "";
 });
